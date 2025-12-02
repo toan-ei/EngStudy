bdef056864078f4a9526d4243fa329afb0f2cac1
TaiyouDang <kiritanitaiyo@gmail.com>
2025-12-02 22:20:27 +0700

    fix listening topic module and create topic dto

--- listening-service/src/listening-topic/dto/create-topic.dto.ts
+++ listening-service/src/listening-topic/dto/create-topic.dto.ts
@@ -1,11 +1,14 @@
 import { IsString, IsOptional, MaxLength } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateTopicDto {
   @IsString()
   @MaxLength(255)
+  @ApiProperty({ description: 'Tên topic', maxLength: 255 })
   topicName: string;
 
   @IsOptional()
   @IsString()
+  @ApiPropertyOptional({ description: 'Mô tả topic' })
   description?: string;
 }
