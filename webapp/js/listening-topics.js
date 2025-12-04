331a983aa5975d32cef937e4f9a55ab09b8e6e17
TaiyouDang <kiritanitaiyo@gmail.com>
2025-12-04 20:40:40 +0700

    connect api listening-topic to frontend

---
+++ webapp/js/listening-topics.js
@@ -0,0 +1,36 @@
+async function loadTopics() {
+    try {
+        const res = await fetch("http://localhost:5001/listening/topics");
+        const topics = await res.json();
+        const container = document.getElementById("topic-container");
+        const iconMap = {
+            "Tranh tả người": "🎨",
+            "Tranh tả vật": "🖼️",
+            "Hội thoại ngắn": "💬"
+        };
+
+        container.innerHTML = "";
+
+        topics.forEach(topic => {
+            const card = document.createElement("button");
+            card.className = "tbi-level-card";
+            card.dataset.id = topic.topicId;
+
+            card.innerHTML = `
+                <div class="tbi-card-icon">${iconMap[topic.topicName] || "🎧"}</div>
+                <p>${topic.topicName}</p>
+            `;
+
+            card.addEventListener("click", () => {
+                window.location.href = `ListeningChooseExercise.html?topicId=${topic.topicId}`;
+            });
+
+            container.appendChild(card);
+        });
+
+    } catch (error) {
+        console.error("Failed to load topics:", error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", loadTopics);
